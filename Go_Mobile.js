7cfe7d8bc509b5868b520ebce8a51048a00fc5be
Elrashid <mohamadali614@gmail.com>
2015-05-08 10:50:48 +0400

    append");

--- Go_Mobile.js
+++ Go_Mobile.js
@@ -11,12 +11,14 @@ var x = $('table');
 		my_Mobile_Stylesheet.type = "text/css";
 		my_Mobile_Stylesheet.href = "http://johnpolacek.github.io/stacktable.js/stacktable.css";
 		$("head").append(my_Mobile_Stylesheet);
+		console.log("stacktable.css append");
 		/////////////////////////////////////////////////
 		my_Mobile_Script = document.createElement("script");
 		$("head").append(my_Mobile_Script);
 		my_Mobile_Script.onload = function() {	$('table').stacktable();console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");	};
 		my_Mobile_Script.type = "text/javascript";
 		my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
+		console.log("stacktable.js append");
 		/////////////////////////////////////////////////
 	}
 });
