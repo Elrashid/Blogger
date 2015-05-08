b9f8d25b7536c99d6c4bf23f372890c01049ea28
Elrashid <mohamadali614@gmail.com>
2015-05-08 07:51:26 +0400

    add new script

---
+++ Go_Mobile.js
@@ -0,0 +1,13 @@
+var my_Mobile_Script = document.createElement("script");
+my_Mobile_Script.type = "text/javascript";
+my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
+$("head").append(my_Mobile_Script);
+
+var my_Mobile_Stylesheet = document.createElement("link");
+my_Mobile_Stylesheet.rel = "stylesheet";
+my_Mobile_Stylesheet.type = "text/css";
+my_Mobile_Stylesheet.href = "http://johnpolacek.github.io/stacktable.js/stacktable.css";
+$("head").append(my_Mobile_Stylesheet);
+
+var x = $('table');
+x.stacktable();
