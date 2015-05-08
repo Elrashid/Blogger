40960ee5833eaee85bfa8c4afa9a9758d03ccd7d
Elrashid <mohamadali614@gmail.com>
2015-05-08 08:10:25 +0400

    update

--- Go_Mobile.js
+++ Go_Mobile.js
@@ -3,7 +3,7 @@ my_Mobile_Script.type = "text/javascript";
 my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
 $("head").append(my_Mobile_Script);
 
-var my_Mobile_Stylesheet = document.createElement("link");
+var my_Mobile_Stylesheet = document.createElement("link"); 
 my_Mobile_Stylesheet.rel = "stylesheet";
 my_Mobile_Stylesheet.type = "text/css";
 my_Mobile_Stylesheet.href = "http://johnpolacek.github.io/stacktable.js/stacktable.css";
