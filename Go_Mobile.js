b28910d3c59fe5b3630d720a3b968f3c7aa12bbe
Elrashid <mohamadali614@gmail.com>
2015-05-08 10:42:25 +0400

    Revert "append the script to the DOM before attaching the onload event"
    This reverts commit  .
    
    Conflicts:
    	Go_Mobile.js

--- Go_Mobile.js
+++ Go_Mobile.js
@@ -12,13 +12,11 @@ var x = $('table');
 		my_Mobile_Stylesheet.href = "http://johnpolacek.github.io/stacktable.js/stacktable.css";
 		$("head").append(my_Mobile_Stylesheet);
 		/////////////////////////////////////////////////
-		$.getScript( "http://johnpolacek.github.io/stacktable.js/stacktable.js", function( data, textStatus, jqxhr ) {
-		  console.log( data ); // Data returned
-		  console.log( textStatus ); // Success
-		  console.log( jqxhr.status ); // 200
-		  console.log( "Load was performed." );
-		  $('table').stacktable()
-		});
+		my_Mobile_Script = document.createElement("script");
+		$("head").append(my_Mobile_Script);
+		my_Mobile_Script.onload = function() {	$('table').stacktable();console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");	};
+		my_Mobile_Script.type = "text/javascript";
+		my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
 		/////////////////////////////////////////////////
 	}
 });
