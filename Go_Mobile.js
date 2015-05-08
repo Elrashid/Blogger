01655de018437bf43400a2fc8e62e0348a4c6912
Elrashid <mohamadali614@gmail.com>
2015-05-08 10:57:04 +0400

    stacktable.js onload

--- Go_Mobile.js
+++ Go_Mobile.js
@@ -14,10 +14,10 @@ var x = $('table');
 		console.log("stacktable.css append");
 		/////////////////////////////////////////////////
 		my_Mobile_Script = document.createElement("script");
-		$("head").append(my_Mobile_Script);
-		my_Mobile_Script.onload = function() {	$('table').stacktable();console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");	};
+		my_Mobile_Script.onload = function() {	$('table').stacktable();console.log("stacktable.js onload");	};
 		my_Mobile_Script.type = "text/javascript";
 		my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
+		$("head").append(my_Mobile_Script);
 		console.log("stacktable.js append");
 		/////////////////////////////////////////////////
 	}
