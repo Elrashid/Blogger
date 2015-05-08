ffe1c29c0266e5330392a96cc09e0aeb9e04ded9
Elrashid <mohamadali614@gmail.com>
2015-05-08 10:11:00 +0400

    refactor

--- Go_Mobile.js
+++ Go_Mobile.js
@@ -1,23 +1,22 @@
-$( document ).ready(function() {
-var my_Mobile_Script = document.createElement("script");
-my_Mobile_Script.type = "text/javascript";
-my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
-$("head").append(my_Mobile_Script);
-
-var my_Mobile_Stylesheet = document.createElement("link"); 
-my_Mobile_Stylesheet.rel = "stylesheet";
-my_Mobile_Stylesheet.type = "text/css";
-my_Mobile_Stylesheet.href = "http://johnpolacek.github.io/stacktable.js/stacktable.css";
-$("head").append(my_Mobile_Stylesheet);
-
-
 var my_Is_Busy = false ; 
 $("body").bind("DOMSubtreeModified", function() {
 var x = $('table');
 	if(! my_Is_Busy && x.length > 0){
 		my_Is_Busy = true ; 
 		console.log(x);
-		x.stacktable();
+
+		/////////////////////////////////////////////////
+		var my_Mobile_Stylesheet = document.createElement("link"); 
+		my_Mobile_Stylesheet.rel = "stylesheet";
+		my_Mobile_Stylesheet.type = "text/css";
+		my_Mobile_Stylesheet.href = "http://johnpolacek.github.io/stacktable.js/stacktable.css";
+		$("head").append(my_Mobile_Stylesheet);
+		/////////////////////////////////////////////////
+		ar my_Mobile_Script = document.createElement("script");
+		my_Mobile_Script.type = "text/javascript";
+		my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
+		my_Mobile_Script.onload = function() {	x.stacktable();	};
+		$("head").append(my_Mobile_Script);
+		/////////////////////////////////////////////////
 	}
-});
 });
