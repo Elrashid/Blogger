efe399a7dcec51f5c1c3720dcb00f725fb830125
Elrashid <mohamadali614@gmail.com>
2015-05-08 09:57:35 +0400

    $( document ).ready(function() {

--- Go_Mobile.js
+++ Go_Mobile.js
@@ -1,3 +1,4 @@
+$( document ).ready(function() {
 var my_Mobile_Script = document.createElement("script");
 my_Mobile_Script.type = "text/javascript";
 my_Mobile_Script.src = "http://johnpolacek.github.io/stacktable.js/stacktable.js";
@@ -9,7 +10,7 @@ my_Mobile_Stylesheet.type = "text/css";
 my_Mobile_Stylesheet.href = "http://johnpolacek.github.io/stacktable.js/stacktable.css";
 $("head").append(my_Mobile_Stylesheet);
 
-$( document ).ready(function() {
+
 var my_Is_Busy = false ; 
 $("body").bind("DOMSubtreeModified", function() {
 var x = $('table');
